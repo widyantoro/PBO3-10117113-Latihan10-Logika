1ad4f79b62e503d03821ffdd6493edee75b50c68
Eka Widyantoro <ekawidyantoro17@gmail.com>
2018-09-23 18:46:47 +0700

    first commit

---
+++ src/pbo3/pkg10117113/latihan10/logika/PBO310117113Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan10.logika;
+
+/*
+ * 
+ * @author
+ * Nama     : Eka Widyantoro
+ * Kelas    : IF-3 / PBO3
+ * NIM      : 10117113
+ * Deskripsi Program : Program 17 tahun keatas
+ */
+public class PBO310117113Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
